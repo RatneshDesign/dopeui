1010745b05052213f1d4929245fed05c13c08bea
RatneshDesgin <220346397+RatneshDesign@users.noreply.github.com>
2025-07-24 21:21:01 +0530

    dev : docs page css & dev...

--- src/Pages/Docs/Docs.jsx
+++ src/Pages/Docs/Docs.jsx
@@ -25,7 +25,7 @@ function Docs() {
 
   // Update document title
   useEffect(() => {
-    document.title = `DopeUI${componentName ? ` - ${componentName}` : ' Documentation'}`;
+    document.title = `DopeUI${componentName ? ` - ${CurrentComponentName}` : ' Documentation'}`;
   }, [componentName]);
 
   // Reset states when component changes
@@ -125,12 +125,23 @@ function Docs() {
     }
   };
 
+  // Determine active component metadata
+  const activeCategory = components.find(cat =>
+    cat.items.some(item => item.slug === componentName?.toLowerCase())
+  );
+
+  const activeComponent = activeCategory?.items.find(item =>
+    item.slug === componentName?.toLowerCase()
+  );
 
   return (
     <div className="docs_container">
 
       <nav className="navbar">
-        <h2>DopeUI Documentation</h2>
+        <Link to={"/"}>
+          <img src="/coloredlogo.svg" alt="logo" style={{ width: "100px" }} />
+        </Link>
+        <a target='_blank' href='https://github.com/RatneshDesign'>Github</a>
       </nav>
 
       <div className="content_wrapper">
@@ -186,30 +197,55 @@ function Docs() {
                       transition={{ duration: 0.2 }}
                       className="category-content"
                     >
-                      {category.items.map(item => (
-                        <motion.div
-                          key={item.slug}
-                          variants={animations.item}
-                          transition={{ duration: 0.15 }}
-                        >
-                          <Link
-                            to={item.path}
-                            className={`component-link ${componentName?.toLowerCase().trim() === item.slug?.toLowerCase().trim()
-                              ? 'active'
-                              : ''
-                              }`}
-                            onClick={() => window.scrollTo(0, 0)}
+                      {category.items.map(item => {
+                        const isActive = componentName?.toLowerCase().trim() === item.slug?.toLowerCase().trim();
+
+                        return (
+                          <motion.div
+                            key={item.slug}
+                            variants={animations.item}
+                            transition={{ duration: 0.15 }}
                           >
-                            <img src="/svgs/arrow.svg" alt="" style={{ marginRight: "5px" }} />
-                            {item.name}
-                            {item.tag && (
-                              <span className={`component-tag ${item.tag}`}>
-                                {item.tag}
-                              </span>
-                            )}
-                          </Link>
-                        </motion.div>
-                      ))}
+                            <Link
+                              to={item.path}
+                              className={`component-link ${isActive ? 'active' : ''}`}
+                              onClick={() => window.scrollTo(0, 0)}
+                            >
+                              <div className="activeactionlink" style={{ marginRight: "5px" }}>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="18"
+                                  height="16"
+                                  viewBox="0 0 18 16"
+                                  fill="none"
+                                >
+                                  <path
+                                    d="M 1 1 V 2.52307 C 1 3.99043 1 4.72411 1.14533 5.338 C 1.94529 8.71723 5.90656 11.3676 10.9574 11.9028 C 11.8749 12 13.8068 12 16 12"
+                                    stroke={isActive ? "white" : "#3f3f3f"}
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                  />
+                                  <path
+                                    d="M14 15C14.6068 14.4102 17 12.8403 17 12C17 11.1597 14.6068 9.5898 14 9"
+                                    stroke={isActive ? "white" : "#3f3f3f"}
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                  />
+                                </svg>
+                              </div>
+                              {item.name}
+                              {item.tag && (
+                                <span className={`component-tag ${item.tag}`}>
+                                  {item.tag}
+                                </span>
+                              )}
+                            </Link>
+                          </motion.div>
+                        );
+                      })}
+
                     </motion.div>
                   )}
                 </AnimatePresence>
@@ -247,7 +283,33 @@ function Docs() {
                     className="component-container"
                     {...animations.fade}
                   >
-                    <h1 className="component-title">{CurrentComponentName}</h1>
+                    <div className="docs_contenant">
+                      <span className="breadcrumb">
+                        <span style={{ color: "#8c8c8c" }}>
+                          Docs
+                        </span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+                          <defs />
+                          <path fill="#ffffffff" d="M8.557,17.396 C8.719,17.274 9.2,16.91 9.478,16.694 C10.034,16.26 10.773,15.666 11.51,15.027 C12.251,14.384 12.972,13.708 13.502,13.108 C13.768,12.807 13.97,12.543 14.102,12.325 C14.226,12.119 14.251,11.998 14.251,11.998 C14.251,11.998 14.226,11.881 14.102,11.675 C13.97,11.457 13.768,11.193 13.502,10.892 C12.972,10.292 12.251,9.617 11.51,8.973 C10.773,8.334 10.034,7.741 9.478,7.306 C9.2,7.09 8.719,6.726 8.558,6.604 C8.224,6.359 8.152,5.889 8.398,5.555 C8.644,5.222 9.113,5.151 9.447,5.396 L9.449,5.398 C9.618,5.526 10.117,5.903 10.401,6.124 C10.97,6.568 11.731,7.178 12.494,7.841 C13.253,8.5 14.032,9.226 14.627,9.899 C14.923,10.235 15.19,10.575 15.386,10.901 C15.571,11.207 15.752,11.594 15.752,12 C15.752,12.407 15.571,12.793 15.386,13.099 C15.19,13.425 14.923,13.765 14.627,14.101 C14.032,14.774 13.253,15.5 12.494,16.159 C11.731,16.822 10.97,17.432 10.401,17.876 C10.117,18.097 9.618,18.474 9.449,18.602 L9.447,18.604 C9.114,18.849 8.644,18.778 8.398,18.445 C8.152,18.111 8.224,17.642 8.557,17.396 Z" />
+                        </svg>
+
+                        {activeCategory?.category}
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+                          <defs />
+                          <path fill="#ffffffff" d="M8.557,17.396 C8.719,17.274 9.2,16.91 9.478,16.694 C10.034,16.26 10.773,15.666 11.51,15.027 C12.251,14.384 12.972,13.708 13.502,13.108 C13.768,12.807 13.97,12.543 14.102,12.325 C14.226,12.119 14.251,11.998 14.251,11.998 C14.251,11.998 14.226,11.881 14.102,11.675 C13.97,11.457 13.768,11.193 13.502,10.892 C12.972,10.292 12.251,9.617 11.51,8.973 C10.773,8.334 10.034,7.741 9.478,7.306 C9.2,7.09 8.719,6.726 8.558,6.604 C8.224,6.359 8.152,5.889 8.398,5.555 C8.644,5.222 9.113,5.151 9.447,5.396 L9.449,5.398 C9.618,5.526 10.117,5.903 10.401,6.124 C10.97,6.568 11.731,7.178 12.494,7.841 C13.253,8.5 14.032,9.226 14.627,9.899 C14.923,10.235 15.19,10.575 15.386,10.901 C15.571,11.207 15.752,11.594 15.752,12 C15.752,12.407 15.571,12.793 15.386,13.099 C15.19,13.425 14.923,13.765 14.627,14.101 C14.032,14.774 13.253,15.5 12.494,16.159 C11.731,16.822 10.97,17.432 10.401,17.876 C10.117,18.097 9.618,18.474 9.449,18.602 L9.447,18.604 C9.114,18.849 8.644,18.778 8.398,18.445 C8.152,18.111 8.224,17.642 8.557,17.396 Z" />
+                        </svg>
+
+                        {activeComponent?.name || slug}
+                      </span>
+
+                      <h1 className="component_title">{activeComponent?.name || 'Component'}</h1>
+
+                      <span className="component_description">
+                        {activeComponent?.description}
+                      </span>
+                    </div>
+
 
                     <div className="view-toggle">
                       <button
--- src/Pages/Docs/Introduction.jsx
+++ src/Pages/Docs/Introduction.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-
+import "./intro.css"
+import { Link } from "react-router-dom"
 function Introduction() {
   return (
-    <div>Introduction</div>
+    <>
+      <h1>
+
+        Introduction
+      </h1>
+
+      <section class="dopeui-intro">
+        <h1><span>DopeUI</span> – Build fast. Build clean.</h1>
+        <p>
+          DopeUI is a lightweight, modern component library for React developers who value speed, flexibility, and aesthetics.
+          Whether you're building a startup MVP or scaling a complex UI system, DopeUI helps you ship faster — without
+          compromising design or performance.
+        </p>
+
+        <ul>
+          <li>⚡ Minimal, fast, and zero-dependency components</li>
+          <li>🎨 Fully customizable with your own styles</li>
+          <li>🧱 Easy-to-use building blocks for modern interfaces</li>
+          <li>📦 Tree-shakable, modular, and production-ready</li>
+        </ul>
+
+        <Link to="/docs/productcard" class="get-started-btn">Get Started</Link>
+      </section>
+
+    </>
   )
 }
 
